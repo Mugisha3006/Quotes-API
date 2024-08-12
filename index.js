75e6d7f71bb5f53f67f119a40f3423e0be53fd4f
mugisha <mohamugi@gmail.com>
2024-08-12 07:12:42 +0300

    install nodemon and add required files

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
-const fs = require('node:fs');
+
 const morgan = require('morgan');
-const path = require('path');
+
 
 const app = express();
 const PORT = 3000;
 
+// apply middleware
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// root url
+app.get('/', (req, res) => {
+    res.send('<h1>Quotes API<h1/>')
+})
+
+// start server
+app.listen(PORT, () => {
+    console.log(`Server is listening on http://localhost:${PORT}`)
+})
