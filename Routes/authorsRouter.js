da4dd34659aee1b5b64147b34d3524b9ceb60cbd
mugisha <mohamugi@gmail.com>
2024-09-01 03:18:40 +0300

    install joi

--- Routes/authorsRouter.js
+++ Routes/authorsRouter.js
@@ -1,23 +1,16 @@
 import { Router } from 'express'
-const router = Router()
-
-import fs from 'node:fs'
 import { getallAuthors, createNewAuthor, getAuthorById, updateAuthorById, deleteAuthorById } from '../Controllers/authorController.js'
 
-// get requests
+const router = Router()
 
 router.get('/', getallAuthors)
-// post requests
 
 router.post('/', createNewAuthor)
 
-// get author by id
 router.get('/:id', getAuthorById)
 
-// update author by id 
 router.patch('/:id', updateAuthorById)
 
-// delete author by id 
 router.delete('/:id', deleteAuthorById)
 
 export default router
